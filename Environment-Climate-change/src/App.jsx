00230075f8b8f38bd6e2e83585ab74dfe8a11313
Baggiyam Shanmugam <gowribaggiyam@gmail.com>
2025-02-17 09:58:52 +0100

    Animal List Update

--- Environment-Climate-change/src/App.jsx
+++ Environment-Climate-change/src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './Components/Navbar'
+import AnimalsList from './Components/AnimalsListing' // Fixed import name
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Navbar/>
+      <Navbar />
       <div>
+        <AnimalsList />
         <h1> Animals Vulnerability Mapping</h1>
       </div>
     </>
---
+++ Environment-Climate-change/src/Components/AnimalsListing.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function AnimalsList() {
+    const [DisplayAnimal, setAnimal] = useState([]);
+
+
+    useEffect(() => {
+
+        axios
+            .get("https://environmentalchanges-5f276-default-rtdb.europe-west1.firebasedatabase.app/animal.json")
+            .then((response) => {
+
+                const animalArray = Object.values(response.data).flat();
+                setAnimal(animalArray);
+            })
+            .catch((error) => {
+                console.error("Error fetching data:", error);
+            });
+    }, []);
+
+
+    return (
+        <div>
+            {DisplayAnimal.map((animal) => (
+                <div key={animal.id}>
+                    <h3>{animal.class}</h3>
+                </div>
+            ))}
+
+
+
+        </div>
+    );
+}
+
+export default AnimalsList;
