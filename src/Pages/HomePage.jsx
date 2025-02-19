af857f79266673b8b434f0591e85fcd99377561b
Baggiyam Shanmugam <gowribaggiyam@gmail.com>
2025-02-19 17:06:19 +0100

    Adding Animals

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -11,24 +11,24 @@ function HomePage(props) {
 
     useEffect(() => {
         axios.get("https://environmentalchanges-5f276-default-rtdb.europe-west1.firebasedatabase.app/animal.json")
-        .then((response) => {
-            const animalArr = Object.keys(response.data).map((id) => ({
-                id,
-                ...response.data[id],
-            }));
-            props.callBackSetAnimal(animalArr)
-        console.log(animalArr)
-        })
-        .catch(e => (console.log("error get request", e)))
+            .then((response) => {
+                const animalArr = Object.keys(response.data).map((id) => ({
+                    id,
+                    ...response.data[id],
+                }));
+                props.callBackSetAnimal(animalArr)
+                console.log(animalArr)
+            })
+            .catch(e => (console.log("error get request", e)))
     }, [])
 
     const geoIcon = new Icon({
-        iconUrl:"src/assets/icon frog.png",
+        iconUrl: "src/assets/icon frog.png",
         iconSize: [38, 38]
     })
-        
+
     return (
-        <> 
+        <>
             <div className="KPI-container">
                 <section className="KPI">
                     <h3>Number of tracked species: {props.callBackDisplayAnimal.length}</h3>
@@ -40,40 +40,40 @@ function HomePage(props) {
                     <h3>Breakdown per species family</h3>
                 </section>
             </div>
-            
-            <MapContainer 
-            className='map-container'
-            center={[13.036272, 10.963980]} 
-            zoom={2}
-            minZoom={2}
-            maxZoom={6}
+
+            <MapContainer
+                className='map-container'
+                center={[13.036272, 10.963980]}
+                zoom={2}
+                minZoom={2}
+                maxZoom={6}
             >
-           
-            <TileLayer
-                attribution= '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-                url="https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.jpg?api_key=5ba27eab-7c1e-480a-b202-79dc8eb15c1a"
-            />
-            {props.callBackDisplayAnimal.map((animal) => {
-                return (
-                    <Marker 
-                    position={animal.geolocation}
-                    icon={geoIcon}>
-                    
-                    <Popup> 
-                        <p>{animal.species}</p>
-                        <Link to={`/animal/${animal.id}`} className="btn btn-outline-secondary">
-                        More details
-                        </Link>
-                    </Popup>
-                    </Marker>
-                )
-            })}
-           
+
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                    url="https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.jpg?api_key=5ba27eab-7c1e-480a-b202-79dc8eb15c1a"
+                />
+                {props.callBackDisplayAnimal.map((animal) => {
+                    return (
+                        <Marker
+                            position={animal.geolocation}
+                            icon={geoIcon}>
+
+                            <Popup>
+                                <p>{animal.species}</p>
+                                <Link to={`/animal/${animal.id}`} className="btn btn-outline-secondary">
+                                    More details
+                                </Link>
+                            </Popup>
+                        </Marker>
+                    )
+                })}
+
 
             </MapContainer>
 
         </>
-        )
+    )
 }
 
 export default HomePage
