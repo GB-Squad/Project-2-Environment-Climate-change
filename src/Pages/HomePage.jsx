32e5c549be3aee13d8d8d4ec508feb93f418ae8b
Gwendal Boedec <boedec.g@gmail.com>
2025-02-19 18:31:44 +0100

    implement first version of charts on homepage

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -5,9 +5,13 @@ import { Icon } from 'leaflet';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import { PieChart, Pie, Cell, Tooltip, Legend } from "recharts";
+
 
 function HomePage(props) {
 
+    const [familyData, setFamilyData] = useState([]);
+    const [statusData, setStatusData] = useState([]);
 
     useEffect(() => {
         axios.get("https://environmentalchanges-5f276-default-rtdb.europe-west1.firebasedatabase.app/animal.json")
@@ -16,14 +20,40 @@ function HomePage(props) {
                 id,
                 ...response.data[id],
             }));
-            props.callBackSetAnimal(animalArr)
-        console.log(animalArr)
+            props.callBackSetAnimal(animalArr);
+            //console.log(animalArr)
+            const animalFamilyCount = {};
+            const animalStatusCount = {};
+            animalArr.forEach((animal) => {
+                const family = animal.family || "unknown";
+                animalFamilyCount[family] = (animalFamilyCount[family] || 0) + 1;
+
+                const status = animal.status || "unknown";
+                animalStatusCount[status] = (animalStatusCount[status] || 0) + 1;
+                
+                const familyFormattedData = Object.keys(animalFamilyCount).map((family) => ({
+                    name: family,
+                    value: animalFamilyCount[family],
+                }));
+
+                const statusFormattedData = Object.keys(animalStatusCount).map((status) => ({
+                    name: status,
+                    value: animalStatusCount[status],
+                }));
+
+                setFamilyData(familyFormattedData);
+                setStatusData(statusFormattedData);
+            })
+            
+     
         })
         .catch(e => (console.log("error get request", e)))
     }, [])
 
+    const COLORS = ["#8884d8", "#82ca9d", "#ffc658", "#ff8042", "#8dd1e1"];
+
     const geoIcon = new Icon({
-        iconUrl:"src/assets/icon frog.png",
+        iconUrl:"public/icon frog.png",
         iconSize: [38, 38]
     })
 
@@ -37,9 +67,46 @@ function HomePage(props) {
                 </section>
                 <section className="KPI">
                     <h3>Breakdown per level of vulnerability</h3>
+                    <PieChart width={400} height={400}>
+                <Pie
+                    data={statusData}
+                    dataKey="value"
+                    nameKey="name"
+                    cx="50%"
+                    cy="50%"
+                    outerRadius={100}
+                    fill="#8884d8"
+                    label
+                >
+                    {statusData.map((entry, index) => (
+                        <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                    ))}
+                </Pie>
+                <Tooltip />
+                <Legend />
+            </PieChart>
                 </section>
                 <section className="KPI">
                     <h3>Breakdown per species family</h3>
+                    <PieChart width={400} height={400}>
+                <Pie
+                    data={familyData}
+                    dataKey="value"
+                    nameKey="name"
+                    cx="50%"
+                    cy="50%"
+                    outerRadius={100}
+                    fill="#8884d8"
+                    label
+                >
+                    {familyData.map((entry, index) => (
+                        <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                    ))}
+                </Pie>
+                <Tooltip />
+                <Legend />
+            </PieChart>
+                    
                 </section>
             </div>
             
