a7a5550a172cf8b0a6c4943e3d030f8e93667fb7
Baggiyam Shanmugam <gowribaggiyam@gmail.com>
2025-02-20 11:27:43 +0100

    NavBar update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,21 +1,36 @@
-import { Link, NavLink } from "react-router-dom"
+import { Link, NavLink, useLocation } from "react-router-dom";
 
 function Navbar() {
-return (
-    <nav className="navbar">
-        <NavLink to="/">
-            Home
-        </NavLink>
-        |
-        <NavLink to="/animalList">
-            Animal List
-        </NavLink>
-        |
-        <NavLink to="/addAnimal">
-            Add a new animal
-        </NavLink>
-    </nav>
-)
+    const location = useLocation();
+    const shouldShowSearchBar = location.pathname !== '/';
+    const shouldShowNavBar = location.pathname === '/animalList' || location.pathname === '/' || location.pathname === '/addAnimal';
+
+    if (!shouldShowNavBar) {
+        return null;
+    }
+
+    return (
+        <div className="container">
+            <nav className="navbar bg-body-tertiary">
+                <NavLink to="/">Home</NavLink> |
+                <NavLink to="/animalList">Animal List</NavLink> |
+                <NavLink to="/addAnimal">Add a new animal</NavLink>
+
+                {shouldShowSearchBar && (
+                    <form className="d-flex">
+                        <input
+                            type="search"
+                            placeholder="Search"
+                            aria-label="Search"
+                        />
+                        <button className="btn btn-outline-success" type="submit">
+                            Search
+                        </button>
+                    </form>
+                )}
+            </nav>
+        </div>
+    );
 }
 
-export default Navbar
+export default Navbar;
